c479a69982998e7acafc25fedbb3ac353f3dc637
AaryanKhClasses <aaryankhclasses07@gmail.com>
2021-07-11 13:05:15 +0530

    Fixed merge issue ig

---
+++ Src/index.js
@@ -0,0 +1,31 @@
+const { register, Client } = require("discord-rpc")
+const { clientID } = require("../config.json")
+const startTimestamp = new Date()
+
+register(clientID)
+
+const client = new Client({
+	transport: "ipc",
+})
+
+client.id = clientID
+
+/**
+ * @param {String} state 
+ * @param {String} details
+ * @param {String} imgText
+ */
+exports.setActivity = async function setActivity(state, details, imgText) {
+	client.setActivity({
+		state: state || 'Having a custom RPC',
+		details: details || 'BaccRPC Maker',
+		startTimestamp,
+		largeImageKey: 'logo-large',
+		largeImageText: imgText || 'BaccRPC Maker',
+		smallImageKey: 'logo-small',
+		smallImageText: 'BaccRPC Maker',
+		instance: false,
+	})
+}
+
+exports.client = client
